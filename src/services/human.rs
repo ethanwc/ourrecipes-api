cf0cdc9abb64d813535a79e6bba3ad2afffc1be3
ethanwc <ethanwc@u.washington.edu>
2020-08-24 22:31:39 -0700

    auth c

--- src/services/human.rs
+++ src/services/human.rs
@@ -1,10 +1,30 @@
 use crate::{
     auth,
     database::Database,
-    models::human::{Human, NewHuman},
+    models::{user::{User, NewUser}, human::{Human, NewHuman}},
 };
 use bson::doc;
 use juniper::{graphql_value, FieldError};
+
+
+pub fn create_user(db: &Database, new_user: NewUser) -> Result<User, FieldError> {
+    let coll = db.collection("user");
+    let serialized_member = bson::to_bson(&new_user)?;
+
+    if let bson::Bson::Document(document) = serialized_member {
+        coll.insert_one(document, None)?;
+        //todo: use actual user id
+        let member_document = coll
+            .find_one(Some(doc! { "id" => "asdf" }), None)?
+            .expect("Document not found");
+
+        let human = bson::from_bson(bson::Bson::Document(member_document))?;
+        Ok(human)
+    } else {
+        println!("Error converting the BSON object into a MongoDB document");
+        Err("Error converting the BSON object into a MongoDB document")?
+    }
+}
 
 pub fn create_human(db: &Database, new_human: NewHuman) -> Result<Human, FieldError> {
     let coll = db.collection("human");
@@ -34,16 +54,16 @@ pub fn get_human(db: &Database, id: &str) -> Result<Vec<Human>, FieldError> {
 
     let mut results: Vec<Human> = vec![];
 
-    let authR = auth();
+    // let authR = auth();
 
-    if !authR.auth {
-        return Err(FieldError::new(
-            "Access requested was declined: 401",
-            graphql_value!({ "internal_error": "Connection refused" }),
-        ));
-    }
+    // if !authR.auth {
+    //     return Err(FieldError::new(
+    //         "Access requested was declined: 401",
+    //         graphql_value!({ "internal_error": "Connection refused" }),
+    //     ));
+    // }
 
-    println!("{}:{}", authR.auth, authR.user_id);
+    // println!("{}:{}", authR.auth, authR.user_id);
 
     for result in cursor {
         match result {
