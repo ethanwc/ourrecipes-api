f7f27180411c91694a2891d2edb693ada4624e70
ethanwc <ethanwc@u.washington.edu>
2020-08-25 02:26:08 -0700

    reduced to add user

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,35 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-use jsonwebtokens_cognito::{Error, KeySet};
-use juniper::RootNode;
-use rocket::{response::content, routes, Rocket, State};
+use rocket::{ routes, Rocket};
 use tokio::runtime::Runtime;
-
-mod database;
-
-
-use database::Database;
+use rocket_contrib::json::Json;
+use mongodb::{Client, Collection};
+use lazy_static::lazy_static;
+use serde::{Deserialize, Serialize};
 
  
 #[macro_use]
 extern crate rocket;
-use serde::Deserialize;
-use rocket_contrib::json::Json;
 
-#[derive(Deserialize)]
-struct User {
+
+lazy_static! {
+    pub static ref MONGO: Client = create_mongo_client();
+}
+
+fn create_mongo_client() -> Client {
+    Client::with_uri_str("mongodb+srv://username:password@cluster.mongodb.net")
+    .expect("Failed to initialize standalone client.")
+}
+
+fn collection(coll_name: &str) -> Collection {
+    MONGO.database("collection").collection(coll_name)
+}
+
+
+
+
+#[derive(Serialize, Deserialize)]
+pub struct NewUser {
     userName: String,
 }
 
@@ -25,13 +37,46 @@ struct User {
  * Add new userid to application database
  */
 #[post("/", data = "<user>")]
-fn add_user(user: Json<User>) {
+fn add_user(user: Json<NewUser>) {
     println!("{}", user.userName);
 }
 
+use bson::doc;
+
+
+async fn test3(new_user: NewUser) -> Result<(), Box<dyn std::error::Error>> { 
+    let coll = collection("user");
+    let serialized_member = bson::to_bson(&new_user)?;
+
+    if let bson::Bson::Document(document) = serialized_member {
+        coll.insert_one(document, None)?;
+        //todo: use actual user id
+        let member_document = coll
+            .find_one(Some(doc! { "id" => "asdf" }), None)?
+            .expect("Document not found");
+
+        let human = bson::from_bson(bson::Bson::Document(member_document))?;
+        Ok(human)
+    } else {
+        println!("Error converting the BSON object into a MongoDB document");
+        Err("Error converting the BSON object into a MongoDB document")?
+    }
+}
+
+
+#[post("/abc", data = "<user>")]
+fn add_user2(user: Json<NewUser>) {
+    let user2 = NewUser {
+        userName: user.userName.to_owned()
+    };
+     Runtime::new()
+    .expect("Failed to create Tokio runtime")
+    .block_on(test3(user2)).expect("asdf");
+}
+
+
 pub fn rocket() -> Rocket {
     rocket::ignite()
-        .manage(Database::init())
         .mount(
             "/",
             routes![add_user],
